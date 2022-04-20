1dc11bd6c6979e9e092874250d72daa0b11fcc3f
clement90 <clement_evrard@hotmail.com>
2022-04-20 19:34:53 +0200

    initialisation du TFE

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e79b4a5517fc85da24d71ec90073f0d6'),
-    },
-  },
-});
